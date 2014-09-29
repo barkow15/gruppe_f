c410e56765333d8507f467a5bbc078b55535d102
Emso13ab <emso13ab@student.cbs.dk>
2014-09-29 13:55:18 +0200

    Scanner tilføjelse

--- server/src/server/ServerMainClass.java
+++ server/src/server/ServerMainClass.java
@@ -7,7 +7,20 @@ import java.util.Scanner;
 
 public class ServerMainClass {
 	private void sysout() {
-		// TODO Auto-generated method stub
-		System.out.println("Andreas var her");
+		Scanner input = new Scanner(System.in);
+		
+		int alder;
+		int malder;
+		int DA;
+		
+		System.out.println("Skriv din alder her");
+		alder = input.nextInt();
+		
+		System.out.println("Skriv din mors alder her");
+		malder = input.nextInt();
+		
+		DA = alder + malder;
+		
+		System.out.println("Jeres alder tilsammen er" + DA );
 	}
 }
