197ad2008576d3c31861ad23e3b603022606e120
MathiasOlsen <Mathiasao@live.dk>
2014-09-29 13:26:09 +0200

    Med client

--- server/src/server/TCPClient.java
+++ server/src/server/TCPClient.java
@@ -1,5 +1,27 @@
 package server;
 
-public class TCPClient {
+import java.io.*;
+import java.net.*;
 
+class TCPClient
+
+{
+	public static void main(String argv[]) throws Exception
+
+	{
+		String sentence;
+		String modifiedSentence;
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		//modtager sætningen fra bruger
+		Socket clientSocket = new Socket("172.17.165.98", 6789); //laver en socket
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		//
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(
+				clientSocket.getInputStream()));
+		sentence = inFromUser.readLine();
+		outToServer.writeBytes(sentence + '\n');
+		modifiedSentence = inFromServer.readLine();
+		System.out.println("FROM SERVER: " + modifiedSentence);
+		clientSocket.close();
+	}
 }
